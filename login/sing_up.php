cf0b080e12fe3456a8e3081c8dccb920ca5591b7
Yedom <yensi31morales@gmail.com>
2025-11-19 15:18:48 -0400

    Creacion del formulario de registro

---
+++ login/sing_up.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+      <link rel="stylesheet" href="../css/style_sing.css">
+   </head>
+   <body>
+
+    <!-- Include the above in your HEAD tag -->
+
+    <div class="sidenav">
+        <div class="login-main-text">
+            <h2>Application<br> Registrarse </h2>
+            <p>Login or register from here to access.</p>
+        </div>
+    </div>
+
+    <div class="main">
+        <div class="col-md-6 col-sm-12">
+            <div class="login-form">
+                <form>
+                    <div class="form-group">
+                        <label>Nombre y Aoellido</label>
+                        <input type="text" class="form-control" placeholder="User Name" id="nomap" name="nomap">
+                    </div>
+                    <div class="form-group">
+                        <label>User Name</label>
+                        <input type="text" class="form-control" placeholder="User Name" id="username" name="username">
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="email" class="form-control" placeholder="User Name" id="email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label>Password</label>
+                        <input type="password" class="form-control" placeholder="Password" id="password" name="password">
+                    </div>
+                        <button type="submit" class="btn btn-black">Login</button>
+                        <button type="submit" class="btn btn-secondary">Register</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+      <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+      <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+   </body>
+</html>
